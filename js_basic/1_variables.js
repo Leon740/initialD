b4f9e35e3a90111925f8546225f463d9db32bc65
Leon740 <dobrinovleonid@gmail.com>
2022-11-12 11:26:59 -0500

    feat(1_variables): add javascript variables examples

---
+++ js_basic/1_variables.js
@@ -0,0 +1,73 @@
+// let number = 0;
+// console.log(number);
+// console.log(typeof(number));
+
+// let string = 'Toyota Supra';
+// console.log(string);
+// console.log(typeof(string));
+
+// let bool = true;
+// console.log(bool);
+// console.log(typeof(bool));
+
+// let nullVar = null;
+// console.log(nullVar);
+// console.log(typeof(nullVar));
+
+// let undefinedVar = undefined;
+// console.log(undefinedVar);
+// console.log(typeof(undefinedVar));
+
+// light switch
+// let light = false;
+
+// if (light == true) {
+//   console.log('Light is ON');
+// } else {
+//   console.log('Light is OFF');
+// }
+
+// automat
+// s = 0 - 20 g = 1
+// s = 20 - 40 g = 2
+// s = 40 - 60 g = 3
+// s = 60 - 80 g = 4
+// s = 80 - 100 g = 5
+
+// const speed = '50';
+// let gear = 0;
+
+// if (typeof(speed) === 'number') {
+//   if (speed > 0 && speed <= 20) {
+//     gear = 1;
+//     console.log('1st');
+//   } else if (speed > 20 && speed <= 40) {
+//     console.log('2nd');
+//     gear = 2;
+//   } else if (speed > 40 && speed <= 60) {
+//     console.log('3rd');
+//     gear = 3;
+//   } else if (speed > 60 && speed <= 80) {
+//     console.log('4th');
+//     gear = 4;
+//   } else if (speed > 80 && speed <= 100) {
+//     console.log('5th');
+//     gear = 5;
+//   } else {
+//     console.log('speed unknown');
+//   }
+// } else {
+//   console.log('wrong type of variable');
+// }
+
+
+// console.log(`speed = ${speed}`);
+// console.log(speed);
+// console.log(`gear = ${gear}`);
+// console.log(gear);
+
+const number = 100;
+const string = '100';
+
+console.log(number == string);
+console.log(number === string);
